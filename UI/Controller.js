f46c3b99ab99e2d0ac5ee5fb8f7656307ec70a81
A.Zed <zohri.alireza@gmail.com>
2018-04-29 18:39:19 +0430

    added UI sketch

---
+++ UI/Controller.js
@@ -0,0 +1,54 @@
+
+var currentWord;
+
+function main() {
+
+}
+
+function createNew() {
+
+}
+
+function loadWord() {
+
+}
+
+function saveLetter() {
+
+}
+
+function cancelProcess() {
+
+}
+
+function addSpline() {
+
+}
+
+function removeSpline() {
+
+}
+
+function selectSpline() {
+
+}
+
+function addPoint() {
+
+}
+
+function removePoint() {
+
+}
+
+function selectPoint() {
+
+}
+
+class Point2D{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    };
+
+}
